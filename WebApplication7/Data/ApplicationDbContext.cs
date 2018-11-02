c7b9366ebea7de805b1591a0bc987267f4e96bbe
Marc Smets <marc.smets@allphi.eu>
2018-11-02 14:54:29 +0100

    Add project files.

---
+++ WebApplication7/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication7.Models;
+
+namespace WebApplication7.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
